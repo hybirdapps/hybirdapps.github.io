fc55e611c62fa45ffed171709f477d28591b6395
Ke Liu <ke.liu@foxmail.com>
2015-01-08 15:38:48 +0800

    add rss app

---
+++ rss/js/reader.js
@@ -0,0 +1,32 @@
+$(function(){
+    var hashtable = {};
+    hashtable['coder'] = "http://weekly.manong.io/issues/";
+    hashtable['python'] = "http://py.memect.com/";
+    hashtable['web'] = "http://web.memect.com/";
+
+    var fetchContents = function(hash){
+        iframe='<iframe src="'+hashtable[hash]+'" seamless="" scrolling="no" frameborder="0" style="width: 100%;" height="800px"></iframe>';
+        $(".container-fluid").html(iframe);
+    };
+
+    var IndexRouterTable = Backbone.Router.extend({
+        routes: {
+            "coder" : "coder",
+            "python" : "python",
+            "web" : "web",
+            "*path": "web"
+        },
+        coder: function(){
+            fetchContents('coder');
+        },
+        python: function(){
+            fetchContents('python');
+        },
+        web: function(){
+            fetchContents('web');
+        }
+    });
+
+    new IndexRouterTable();
+    Backbone.history.start();
+});
